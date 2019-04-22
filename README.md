5bfdd83a846bedb36ee9bcbbcdfcffce2a715cf4
zettat123 <zettat123@gmail.com>
2019-04-22 12:59:34 +0800

    Modified Readme.md

--- README.md
+++ README.md
@@ -1,6 +1,14 @@
 # iot-host-status-monitor
 
+## How to run?
+
 ```
 npm i
 npm start
+```
+
+## How to build?
+
+```
+npm run build
 ```
--- src/url.js
+++ src/url.js
@@ -1,9 +1,9 @@
-let url = ""
-if (process.env.NODE_ENV === "development") {
-  url = "http://dev.bz.com/api"
-  require("../mock/index")
+let url = ''
+if (process.env.NODE_ENV === 'development') {
+  url = 'http://dev.bz.com/api'
+  require('../mock/index')
 } else {
-  url = "" // TODO: Fill with real url.
+  url = 'http://bz302.com' // TODO: Fill with real url.
 }
 
 export default url
